900b02ff0c1ad55141a9eb18c63a01ddb0bff055
Pavel Grubeljas <pagrub@ttu.ee>
2022-03-12 16:08:30 +0200

    inputs

---
+++ associationRule/xls_reader.py
@@ -0,0 +1,14 @@
+import xlrd
+book = xlrd.open_workbook("tshekid.xls")
+sh = book.sheet_by_index(0)
+
+prev_index = 1.0
+with open('input2.txt', 'w') as writer:
+    for rx in range(1, sh.nrows):
+        row = sh.row(rx)[:2]
+        index = row[0].value
+        product = row[1].value
+        if prev_index != index:
+            writer.write('\n')
+            prev_index = index
+        writer.write(product + " ")
